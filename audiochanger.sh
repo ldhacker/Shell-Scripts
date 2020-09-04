f424c9eec8810580a941c82318730ef2bb178157
Dustin Hacker <ldhacker@live.com>
2020-09-04 17:40:04 +0900

    Update audiochanger.sh
    Changed to pull the Current I/O state of the system.

--- audiochanger.sh
+++ audiochanger.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 while :
 do
-echo "Which audio source would you like?"
-echo "0. hdmi-stereo"
-echo "1. Kingston_HyperX_Cloud_Flight_Wireless_Headset"
-echo "2. iec958-stereo"
-echo "Enter a number below (press ctrl+C to exit):"
+pacmd list-sinks | grep -E "name:|index:"	
+echo "Enter a number below to make a selection."
+echo "Press ctrl+C to exit."
 read CHOICE
 pacmd set-default-sink $CHOICE
-echo "You're Audio Output has been set to $CHOICE"
+clear
+echo "Your Audio Output has been set to $CHOICE"
+echo "Press ctrl+C to exit."
 echo "Press enter to make another selection."
-echo "Otherwise, press ctrl+C to exit."
 read NOTHING
 clear
 done
